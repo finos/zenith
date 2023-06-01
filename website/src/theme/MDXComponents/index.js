00760a5ef1ee3fe7bfad9b4fd50e6d3f35597582
Peter Smulovics <psmulovics@users.noreply.github.com>
2023-06-01 15:35:21 -0400

    adding box

--- website/src/theme/MDXComponents/index.js
+++ website/src/theme/MDXComponents/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import MDXComponents from '@theme-original/MDXComponents';
 import Bio from '../Bio';
+import BoxOut from '../BoxOut';
 
 export default {
   ...MDXComponents,
-  Bio: Bio
+  Bio: Bio,
+  BoxOut: BoxOut
 };
