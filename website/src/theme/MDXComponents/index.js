ca6623ef0e989e52da40d8670cedfa97154a9d40
Patrick Downing <19316277+padraignix@users.noreply.github.com>
2023-07-26 17:25:25 -0400

    adding mdxcomponent

--- website/src/theme/MDXComponents/index.js
+++ website/src/theme/MDXComponents/index.js
@@ -3,10 +3,12 @@ import MDXComponents from '@theme-original/MDXComponents';
 import Bio from '../Bio';
 import BoxOut from '../BoxOut';
 import SideBySide from '../SideBySide';
+import PicCenter from '../PicCenter';
 
 export default {
   ...MDXComponents,
   Bio: Bio,
   BoxOut: BoxOut,
-  SideBySide: SideBySide
+  SideBySide: SideBySide,
+  PicCenter: PicCenter
 };
