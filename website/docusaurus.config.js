84c468cd70f1b9142cb201229e46c8b88f1859c6
Peter Smulovics <psmulovics@users.noreply.github.com>
2026-04-22 15:50:27 -0400

    Remove Blog link from Docusaurus config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -46,8 +46,7 @@ module.exports = {
           href: 'https://github.com/finos/zenith',
           label: 'GitHub',
           position: 'right',
-        },
-        {to: 'blog', label: 'Blog', position: 'right'}
+        }
       ],
     },
     footer: {
